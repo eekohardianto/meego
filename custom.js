78c51b6e991621ec9615b25967e8eab02d1e4831
eekohardianto <46664411+eekohardianto@users.noreply.github.com>
2019-08-09 10:44:48 +0700

    Delete custom.js

--- custom.js
+++
@@ -1,7 +0,0 @@
-history.pushState(null, null, '#');
-window.addEventListener('popstate', function(event)
-{
-history.pushState(null, null, '#');
-});
-
-(function(window, location) {history.replaceState(null, document.title, location.pathname+"#!/history");history.pushState(null, document.title, location.pathname);window.addEventListener("popstate", function() {if(location.hash === "#!/history") {history.replaceState(null, document.title, location.pathname);setTimeout(function(){location.replace("https://www.meego.id/");},0);}}, false);}(window, location));
